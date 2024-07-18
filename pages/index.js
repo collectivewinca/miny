661a7e1e23d9605beac215cc6d9ba93178e95f3c
collectivewinca <159792879+collectivewinca@users.noreply.github.com>
2024-07-18 17:05:55 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from './components/Layout'; // Adjust the path if needed
+import Layout from './components/layouts'; // Adjust the path if needed
 
 export default function Home() {
   return (
