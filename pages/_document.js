34e075492eebec14a44c97fb94b2afa8409cffcb
collectivewin <hello@collectivewin.ca>
2024-07-18 21:43:10 +0530

    pages

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,19 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        {/* Preconnect to the fonts.gstatic.com domain */}
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="true" />
+        {/* Load the Google Fonts asynchronously */}
+        <link
+          rel="stylesheet"
+          as="style"
+          onLoad="this.onload=null;this.rel='stylesheet'"
+          href="https://fonts.googleapis.com/css2?display=swap&amp;family=Be+Vietnam+Pro%3Awght%40400%3B500%3B700%3B900&amp;family=Noto+Sans%3Awght%40400%3B500%3B700%3B900"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
