aa3fa9a1a57e3ac2ba6a30631282bff87d02f8c8
collectivewinca <159792879+collectivewinca@users.noreply.github.com>
2024-07-18 17:16:32 -0400

    Update howitworks.js

--- pages/howitworks.js
+++ pages/howitworks.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../components/Layout'; // Adjust the path if needed
+import Layout from '../components/Layout'; 
 
 function HowItWorks() {
   return (
