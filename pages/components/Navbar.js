2c4f09f76d0a448cff8bb5bbff431ccaab08224c
collectivewinca <159792879+collectivewinca@users.noreply.github.com>
2024-07-18 17:24:47 -0400

    Create Navbar.js

---
+++ pages/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+
+function Navbar() {
+  return (
+    <nav> 
+      {/* Your navigation links from Hero.js */}
+    </nav>
+  );
+}
+
+export default Navbar;
