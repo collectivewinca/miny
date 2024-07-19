a062ceef7d6bc3d219d2cbc3ac8c87f06a243778
collectivewin <hello@collectivewin.ca>
2024-07-19 16:44:41 +0530

    fix

--- pages/howitworks.js
+++ pages/howitworks.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../components/Layout'; // Ensure the path is correct
+import Layout from '@/components/Layout'; // Ensure the path is correct
 
 function HowItWorks() {
   return (
